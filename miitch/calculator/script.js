18d29cb425e3aa3469fdf6a832f3a3707d22b726
miitch <miitch@inven.co.kr>
2021-06-05 21:12:02 +0900

    calculator 2222

--- miitch/calculator/script.js
+++ miitch/calculator/script.js
@@ -1,85 +1,131 @@
 'use strict';
+/**
+ * data
+ * item
+ */
+let lastData, typeOfItem;
+let data = "";
+let lastType = "";  // 부호가 연속으로 입력되는 것을 방지하기 위해서 사용
+let lastIsMinus = "";
+let beforeLastType = "";
+let isAdd = 1;  // 뒤에 더할지, 더하지 않고 마지막 글자를 덮어쓸지 체크
+let jobDone = 0;  // = 버튼을 눌러서 계산이 끝나면 1;
 
-let data, dataAry = []
 let buttons = document.querySelectorAll('.button');
 let inputArea = document.querySelector('.input');
 let outputArea = document.querySelector('.output');
 
 buttons.forEach(item => {
 	item.addEventListener('click', event => {
-		getData(item);
+		doCalc(item);
 	})
 })
 
-function inputDataList(item) {
-	if(data == undefined) {
-		data = item.innerHTML;
+function doCalc(item) {  //타입값에 따라 분기
+	typeOfItem = item.getAttribute('data-type');
+
+	switch(typeOfItem) {
+		case 'equal' : calData(); break;
+		case 'clear' : clearData(); break;
+		case 'backspace' : delData(); break;
+		default : updateData(typeOfItem, item); break;
+	}
+}
+
+
+/**
+ * 1. 부호 뒤의 부호는 덮어쓴다.
+ *    단, 마이너스 외 부호 뒤에 마이너스를 넣을 때는 추가한다. ( 50 * -10 )
+ * 2. 현재의 마지막 문자가 숫자가 아닐 때 . 을 입력하려고 하면 . 앞에 자동으로 0 을 추가한다
+ */
+function updateData(typeOfItem, item) {
+	isAdd = 1;  // 1: 추가, 0: 덮어씀
+	isDot = item.getAttribute('data-type2') == 'dot';
+	if(typeOfItem != 'num' && data != undefined) {
+		checkLastType();
+	} else if(typeOfItem != 'num') {
+		alert('숫자를 먼저 입력하세요.');
+		return;
+	}
+	
+	if(typeOfItem != 'num' && lastType == 0) {  // 넣으려는 값이 숫자가 아니고, 현재 마지막 문자가 부호일 때: 덮어쓴다
+		if(typeOfItem == 'minus' && lastIsMinus != 0) {  // 단, 넣으려는 값이 마이너스부호이고, 마지막 문자가 마이너스 아닌 부호일 때: 추가한다
+			isAdd = 1;
+		} else {
+			isAdd = 0;
+		}
+	}
+
+	if(isDot == true) {
+		if (lastIsDot == 0) {
+			isAdd = 0;
+		} else if (lastIsNum != 0) {
+			addZero = 1;
+		}
+		/**
+		 * isAdd 가 0 이어야 하는 케이스
+		 * 라스트타입이 . 일 때 => lastIsDot
+		 * 
+		 * 0을 붙여줘야 하는 케이스
+		 * 라스트타입이 num이 아닐 때 => lastIsNum
+		 */
+	}
+
+	inputDataList(item, isAdd);
+	// calData();  // <-----
+}
+
+function checkLastType() {
+	lastType = +data[data.length - 1].search(/[\+\-\*\/]/);
+	lastIsMinus = +data[data.length - 1].search(/[\-]/);
+	lastIsDot = +data[data.length - 1].search(/[\.]/);
+	lastIsNum = +data[data.length - 1].search(/[0-9]/);
+}
+let addZero;
+function inputDataList(item, isAdd, addZero = 0) {
+	let r = item.innerHTML;
+	lastData = data[data.length - 1];
+	if(!isAdd) {
+		data = data.replace(lastData, r);
+		if(addZero) {
+			//<-----
+		}
+
 	} else {
-		data += item.innerHTML;
+		if(data == undefined) {
+			data = r;
+		} else {
+			data += r;
+		}
 	}
 	inputDisplay(data);
-	console.log(data);
 }
 
 function inputDisplay(data) {
 	inputArea.innerHTML = data;
 }
 
-let dataList;
-let lastType = "num";
-
-function getData(data) {  //타입값에 따라 분기
-	let typeOfData = data.getAttribute('data-type');
-	console.log(typeOfData);
-
-
-/**
- * 부호를 연속으로 입력할 때 
- * 기본적으로 부호 뒤에 또 부호를 쓰면 나중에 쓴 부호로 교체된다.
- * 예외 - 이외의 부호 뒤에 입력된 - 는 앞 부호 뒤에 그대로 추가된다.
- * 상기 예외 케이스에서 - 를 다른 부호로 교체하려고 한 경우 부호가 삭제된다.
- */
-	switch(typeOfData) {
-		case 'num' : 
-			inputDataList(data);
-			lastType = 'num';
-		break;
-
-		case 'minus' :
-			if(lastType != 'num') {
-				checkSign(typeOfData);
-			} else {
-				inputDataList(data);
-			}
-			lastType = 'minus';
-		break;
-
-		case 'plus' : break;
-		case 'multiply' : break;
-		case 'obelus' : break;
-		case 'equal' : break;
-		case 'clear' : break;
-		case 'backspace' : break;
-	}
-}
-
-
-function checkSign(type) {  // 부호를 확인함
-	return;
-} 
-
 function calData() {  //계산
+	let result = eval(data);
+	returnData(result);
 }
 
-function returnData() {  // 계산한 값을 돌려줌
+function returnData(result) {  // 계산한 값을 돌려줌
+	outputArea.innerHTML = result;
+	jobDone = 1;
 }
 
-function clearData() {  // 모든것을 초기화한다
+function clearData() {  //초기화
+	data = "";
+	lastType = "";
+	inputArea.innerHTML = "";
+	outputArea.innerHTML = "";
+	jobDone = 0;
 
 }
 
 function delData() {  //직전에 입력한 값을 지운다
-}
-
-function alertOver() {  // 수가 너무 크면
+	if(jobDone == 1) alert("이미 계산이 끝났습니다. clear 버튼으로 초기화 하세요.");
+	data = data.slice(0, -1);
+	inputDisplay(data);
 }
