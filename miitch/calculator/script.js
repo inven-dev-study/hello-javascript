4f159670b11b74206519dfbfb146df82b9808070
Miitch <miitch@inven.co.kr>
2021-06-08 14:57:59 +0900

    calculator 3333

--- miitch/calculator/script.js
+++ miitch/calculator/script.js
@@ -1,19 +1,31 @@
 'use strict';
 /**
- * data
- * item
+ * 문제점
+ *  0.1 + 0.2 != 0.3
+ *  4 * -    ===>   4 * *    ===> 4 * * -    ===> 4 * * *  ...
  */
-let lastData, typeOfItem;
-let data = "";
-let lastType = "";  // 부호가 연속으로 입력되는 것을 방지하기 위해서 사용
-let lastIsMinus = "";
-let beforeLastType = "";
-let isAdd = 1;  // 뒤에 더할지, 더하지 않고 마지막 글자를 덮어쓸지 체크
-let jobDone = 0;  // = 버튼을 눌러서 계산이 끝나면 1;
 
-let buttons = document.querySelectorAll('.button');
-let inputArea = document.querySelector('.input');
-let outputArea = document.querySelector('.output');
+let data = "",  // 입력한 데이터(string)
+	typeOfItem;  // 마지막으로 입력한 데이터의 data-type
+
+// 부호가 연속으로 입력되는 것을 방지하기 위해서 사용
+let lastIsSymbol = -1,
+	lastIsMinus = -1,
+	lastIsDot = -1,
+	lastIsNum = 0;
+
+let isAdd = 1;  // 값을 뒤에 추가할지, 추가하지 않고 마지막 문자를 덮어쓸지 체크
+
+// 소숫점, 0 입력관련
+let isDot = 0,
+	isZero = 0,
+	afterDot = 0,  // 소수점 뒤인지 판단  0: 소수점앞, 1: 소수점뒤
+	addZero = 1;  //0을 입력할 수 있는가  0: 불가, 1: 가능
+
+let buttons = document.querySelectorAll('.button'),
+	inputArea = document.querySelector('.input'),
+	outputArea = document.querySelector('.output');
+
 
 buttons.forEach(item => {
 	item.addEventListener('click', event => {
@@ -32,67 +44,70 @@ function doCalc(item) {  //타입값에 따라 분기
 	}
 }
 
-
 /**
- * 1. 부호 뒤의 부호는 덮어쓴다.
- *    단, 마이너스 외 부호 뒤에 마이너스를 넣을 때는 추가한다. ( 50 * -10 )
- * 2. 현재의 마지막 문자가 숫자가 아닐 때 . 을 입력하려고 하면 . 앞에 자동으로 0 을 추가한다
+ * 부호 뒤의 부호는 덮어쓴다.
+ * 단, 마이너스 외 부호 뒤에 마이너스를 넣을 때는 추가한다. ( 50 * -10 )
  */
+
 function updateData(typeOfItem, item) {
 	isAdd = 1;  // 1: 추가, 0: 덮어씀
 	isDot = item.getAttribute('data-type2') == 'dot';
-	if(typeOfItem != 'num' && data != undefined) {
-		checkLastType();
+	isZero = item.getAttribute('data-type2') == 'zero';
+	checkLastType();
+
+	if(typeOfItem != 'num' && data) {
+		afterDot = 0;
+		addZero = 1;
 	} else if(typeOfItem != 'num') {
 		alert('숫자를 먼저 입력하세요.');
 		return;
 	}
-	
-	if(typeOfItem != 'num' && lastType == 0) {  // 넣으려는 값이 숫자가 아니고, 현재 마지막 문자가 부호일 때: 덮어쓴다
-		if(typeOfItem == 'minus' && lastIsMinus != 0) {  // 단, 넣으려는 값이 마이너스부호이고, 마지막 문자가 마이너스 아닌 부호일 때: 추가한다
-			isAdd = 1;
-		} else {
-			isAdd = 0;
-		}
+
+	if(typeOfItem != 'num' && lastIsSymbol == 0) {  // 넣으려는 값이 숫자가 아니고, 현재 마지막 문자가 부호일 때: 덮어쓴다.
+		isAdd = (typeOfItem == 'minus' && lastIsMinus != 0) ? 1 : 0;  //단, 넣으려는 값이 마이너스 부호이고, 마지막 문자가 마이너스가 아닌 부호일 때: 추가한다
 	}
 
-	if(isDot == true) {
-		if (lastIsDot == 0) {
-			isAdd = 0;
-		} else if (lastIsNum != 0) {
-			addZero = 1;
+	if(isDot) {
+		if (!afterDot) {
+			isAdd = (lastIsDot == 0) ? 0 : isAdd;
+		} else {
+			return;
 		}
-		/**
-		 * isAdd 가 0 이어야 하는 케이스
-		 * 라스트타입이 . 일 때 => lastIsDot
-		 * 
-		 * 0을 붙여줘야 하는 케이스
-		 * 라스트타입이 num이 아닐 때 => lastIsNum
-		 */
+		afterDot = 1;
+		addZero = 1;
+	}
+
+	if (isZero) {
+		if (addZero && !afterDot) {
+			addZero = 0;
+		} else if (!addZero) {
+			return;
+		}
 	}
 
 	inputDataList(item, isAdd);
-	// calData();  // <-----
+
+	if(lastIsNum) {
+		calData();
+	}
 }
 
 function checkLastType() {
-	lastType = +data[data.length - 1].search(/[\+\-\*\/]/);
-	lastIsMinus = +data[data.length - 1].search(/[\-]/);
-	lastIsDot = +data[data.length - 1].search(/[\.]/);
-	lastIsNum = +data[data.length - 1].search(/[0-9]/);
+	if(data){
+		lastIsSymbol = +data[data.length - 1].search(/[\+\-\*\/]/);
+		lastIsMinus = +data[data.length - 1].search(/[\-]/);
+		lastIsDot = +data[data.length - 1].search(/[\.]/);
+		lastIsNum = +data[data.length - 1].search(/[0-9]/);
+	}
 }
-let addZero;
-function inputDataList(item, isAdd, addZero = 0) {
-	let r = item.innerHTML;
-	lastData = data[data.length - 1];
-	if(!isAdd) {
-		data = data.replace(lastData, r);
-		if(addZero) {
-			//<-----
-		}
 
+function inputDataList(item, isAdd) {
+	let r = item.innerHTML;
+	if(!isAdd) {
+		data = data.slice(0, -1);
+		data += r;
 	} else {
-		if(data == undefined) {
+		if(!data) {
 			data = r;
 		} else {
 			data += r;
@@ -104,28 +119,59 @@ function inputDataList(item, isAdd, addZero = 0) {
 function inputDisplay(data) {
 	inputArea.innerHTML = data;
 }
-
+let result, dataForResult;
 function calData() {  //계산
-	let result = eval(data);
-	returnData(result);
+	if(data) {
+		result = dataForResult = data;
+
+		check:
+		for(var i = 0; i < (data.length - 1); i++) {
+			checkLastType();
+			if(lastIsNum != 0) {
+				dataForResult = data.slice(0, -1);
+				break check;
+			}
+		}
+
+		try {
+			dataForResult = eval(dataForResult);
+
+		} catch(e) {
+			dataForResult = '잘못된 수식';
+			returnData(dataForResult);
+			return;
+		}
+
+		inputDisplay(result);
+		returnData(dataForResult);
+
+	}
 }
 
-function returnData(result) {  // 계산한 값을 돌려줌
-	outputArea.innerHTML = result;
-	jobDone = 1;
+function returnData(result) {
+	if(result) {
+		outputArea.innerHTML = result;
+	}
 }
 
-function clearData() {  //초기화
+function clearData() {
 	data = "";
-	lastType = "";
+	lastIsSymbol = "";
 	inputArea.innerHTML = "";
 	outputArea.innerHTML = "";
-	jobDone = 0;
-
+	afterDot = 0;
+	addZero = 1;
 }
 
-function delData() {  //직전에 입력한 값을 지운다
-	if(jobDone == 1) alert("이미 계산이 끝났습니다. clear 버튼으로 초기화 하세요.");
+function delData() {
+	// 0, 소숫점 입력관련
+	checkLastType();
+	if (lastIsDot == 0) {
+		afterDot = 0;
+		addZero = 1;
+	}
+
 	data = data.slice(0, -1);
 	inputDisplay(data);
+	calData();
 }
