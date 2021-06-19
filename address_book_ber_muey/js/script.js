45f34c13adb430ee2f2aecf93dc99f6521feada6
door0 <68139125+door0@users.noreply.github.com>
2021-06-20 00:03:22 +0900

    중간 커밋

--- address_book_ber_muey/js/script.js
+++ address_book_ber_muey/js/script.js
@@ -26,7 +26,6 @@ let yesBtn = document.getElementById("delete-yes");
 let closeBtns = document.getElementsByClassName("close");
 
 
-
 // 수정 여부
 let isUpdateMode = false;
 
@@ -48,15 +47,115 @@ let users = {
   },
 };
 
+// detailinfo 들어갈 때 예시 작성
+// for(let i=0;i<5;i++){
+//  var elem=document.createElement('div');
+//  elem.appendChild(document.createTextNode('The man who mistook his wife for a hat'));
+//  document.getElementById('personinfo').appendChild(elem)
+// }
+
+// 객체를 배열에 담음
+let arr=[
+  {
+  name: "베르",
+    phone: "01012345678",
+    address: "서울",
+    email: "ber@inven.co.kr",
+    isFavorite: false,
+  },{
+    name: "뮤이",
+    phone: "01012345678",
+    address: "서울",
+    email: "muey@inven.co.kr",
+    isFavorite: false,
+  },
+];
+
+// 한글 초성 빼기
+function cho_hangul(hangeul,str) {
+  let result= "";
+  for( i = 0; i < 1; i++) {
+    let code = str.charCodeAt(i)-44032;
+
+    if(code>-1 && code<11172){
+      let chosung=hangeul[Math.floor(code/588)]
+      result=chosung;
+    }
+  }
+return result;
+}
+
+
+var hangeul =  ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"];
+
+  for ( var i=0; i < hangeul.length; i++) {
+    // 노란색 자음 바 반복문
+    let letter=hangeul[i];
+
+    // personlist 자식요소 생성
+    let nameElem=document.createElement('div')
+    nameElem.setAttribute("class", "detailinfo");
+
+
+    for(let j=0;j<arr.length;j++){
+
+      try {
+      // 주석 친 부분을 사용하면 ㄱ 이 없어지는 오류..가 있어 일단 주석처리
+      //   let name=arr[i].name
+      
+      // let firstLetter=cho_hangul(hangeul,name)
+      // // let firstLetter=cho_hangul(hangeul,'가오')
+      // console.log('firstLetter: ',firstLetter)
+
+      // if(firstLetter==letter){ // 객체 이름의 초성과 한글 배열의 값이 같다면
+      
+      //   console.log('yeah: ',letter)
+      //   console.log('yeah2: ',name)
+      // }else{
+      //   console.log('yeah3: ',letter)
+      //   console.log('yeah4: ',name)
+      // }
+
+      let br = document.createElement("br");
+      nameElem.appendChild(document.createTextNode(`${arr[j].name}`));
+      nameElem.appendChild(br);
+    
+      } catch (error) {        
+        console.log('error: ',error)
+      }
+    } 
+
+    // <div class ="personlist" id="person_0" > 생성
+    var elem=document.createElement('div')
+    
+    elem.appendChild(document.createTextNode(`${hangeul[i]}`));
+    elem.setAttribute('class','personlist')
+    elem.setAttribute('id',`person_${i}`)
+
+    elem.appendChild(nameElem)
+
+    document.getElementById('personListWrapper').appendChild(elem)
+    // document.getElementById('personlist').innerHTML =  `<div class="personlist">${hangeul[i]}</div>`;
+
+  }
+
+
 window.onload = function(){
-  
   for(let btn of closeBtns){
     btn.onclick = function() {
       closeModal();
     }
   }
-  
 };
+
+// user toggle, 이름을 클릭하면 detailinfo 폴딩 동작 (수정필요), 이 부분 주석 풀면 addBtn이 동작하지 않음.
+// toggleBtn.onclick = function() {
+//   if(document.getElementById('detailinfo').style.display === 'none') {
+//     document.getElementById('detailinfo').style.display = 'block';
+//   } else {
+//     document.getElementById('detailinfo').style.display = 'none';
+//   }
+// }
 
 
 // When the user clicks on the button, open the modal
@@ -66,28 +165,99 @@ addBtn.onclick = function () {
   clearInputDatas([userName, phone, email, address]);
 };
 
+// 처음에 form에 submit 새로고침 이슈 때문에 saveBtn을 만들어놨는데,
+// 이벤트리스너가 있더라고용 ! (제가 쓴 코드가 아니여서 알아보기가 어렵고
+// 또 주말인데 여쭤보기가 좀 불편해하실까봐 여기에 모든 것을.. 적습니당 ) 
+// 뭐가 좀더 나은지 알아봤는데 submit 이벤트 리스너는 스크롤 리스닝이나 소켓통신할 때 쓰인대서,
+// saveBtn을 만들어봤는데, 수정하기 어렵거나 복잡하다면 submit 이벤트 리스너 그대로
+// 하는게 좋겠다고 생각합니당. 
+saveBtn.onclick = function (e) {
+  e.preventDefault()
+  let name=document.getElementById('userName').value
+  let phone=document.getElementById('phone').value
+  let email=document.getElementById('email').value
+  let address=document.getElementById('address').value
+
+  
+  console.log('name: ',name)
+  console.log('phone: ',phone)
+  console.log('email: ',email)
+  console.log('address: ',address)
+
+  let obj={
+    name: name,
+    phone: phone,
+    address:address,
+    email: email,
+    isFavorite: false,
+  }
+
+  if(obj != null) {
+    arr.push(obj);
+    document.querySelector(".detailinfo").value="";
+  } 
+
+  showList();
+
+  // location.reload();
+  //   isUpdateMode = false;
+  // modal.style.display = "block";
+  // clearInputDatas([userName, phone, email, address]);
+};
+
+document.getElementById('personinfo').appendChild = `
+<div id="personinfo-element" class="personinfo-element" >
+    <div class="toggle-name" id="toggleBtn" >
+        <img class="profileimg" src="/icon/person.svg"/>
+        <span>${arr.name}</span>
+    </div>
+    <div class="detailinfo" id="detailinfo" style="display: none;">
+        <p>Name : ${arr.name}</p>
+        <p>Phone : ${arr.phone}</p>
+        <p>Address : ${arr.address}</p>
+        <p>Email : ${arr.email}</p>
+        <button id="updateBtn">Update User</button>
+        <button id="favoriteBtn">즐겨찾기</button>
+        <button id="deleteBtn">삭제</button>
+    </div>
+</div>
+`;
+
+function showList() {
+  let list = "<div id='personinfo-element'>";
+
+  for (let i = 0; i < arr.length; i++) {
+    
+  }
+}
+
+
 updateBtn.onclick = function() {
     isUpdateMode = true;
     modal.style.display = "block";
     let user = getUser('베르_123');
     displayUser(user);
-    console.log(users);
+    //console.log(users);
 }
 
 favoriteBtn.onclick = function() {
     updateFavorite('베르_123', true);
-    console.log(users);
+    //console.log(users);
 }
 deleteBtn.onclick = function(){
   isUpdateMode = false;
   deleteModal.style.display = "block";
-  console.log(users);
+  //console.log(users);
+  arr.splice(userName, 1);
+  showList();
 }
 
-function displaySubmitButton(text){
-    let saveBtn = document.querySelector('[type=submit]')
-    saveBtn.innerText = text;
-}
+
+
+// function displaySubmitButton(text){
+//     let saveBtn = document.querySelector('[type=submit]')
+//     saveBtn.innerText = text;
+// }
 
 function displayUser(user){
     userId.value = user.id;
@@ -364,19 +534,6 @@ yesBtn.onclick = function(){
 }
 
 // 이미지??
-
-// user toggle
-toggleBtn.onclick = function() {
-  if(document.getElementById('detailinfo').style.display === 'none') {
-    document.getElementById('detailinfo').style.display = 'block';
-  } else {
-    document.getElementById('detailinfo').style.display = 'none';
-  }
-}
-
-
-// 저장한 객체 보여주기
-
 
 
 
